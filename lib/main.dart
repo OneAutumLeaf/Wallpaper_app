e408aa17897c0723506820caaa16a34545c4031a
Ayush Singh <ayushsingh12072000@iiitsonepat.ac.in>
2022-03-30 19:44:53 +0530

    Final Commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:wallpaper/wallpaper.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      home: Wallpaper(),
+    );
+  }
+}
---
+++ lib/wallpaper.dart
@@ -0,0 +1,103 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'fullscreen.dart';
+
+class Wallpaper extends StatefulWidget {
+  @override
+  _WallpaperState createState() => _WallpaperState();
+}
+
+class _WallpaperState extends State<Wallpaper> {
+  List images = [];
+  int page = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchapi();
+  }
+
+  fetchapi() async {
+    await http.get(Uri.parse('https://api.pexels.com/v1/curated?per_page=80'),
+        headers: {'Authorization': '563492ad6f91700001000001fb8310a8d15f482ea6898ac0e01bd658'}).then((value) {
+      Map result = jsonDecode(value.body);
+      setState(() {
+        images = result['photos'];
+      });
+      print(images[0]);
+    });
+  }
+
+  loadmore() async {
+    setState(() {
+      page = page + 1;
+    });
+    String url =
+        'https://api.pexels.com/v1/curated?per_page=80&page=' + page.toString();
+    await http.get(Uri.parse(url), headers: {'Authorization': '563492ad6f91700001000001fb8310a8d15f482ea6898ac0e01bd658'}).then(
+            (value) {
+          Map result = jsonDecode(value.body);
+          setState(() {
+            images.addAll(result['photos']);
+          });
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:  AppBar(title: Text("Wallpapers"),
+        centerTitle: true,),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              child: GridView.builder(
+                  itemCount: images.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisSpacing: 2,
+                      crossAxisCount: 3,
+                      childAspectRatio: 2 / 3,
+                      mainAxisSpacing: 2),
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => FullScreen(
+                                  imageurl: images[index]['src']['large2x'],
+                                )));
+                      },
+                      child: Container(
+                        color: Colors.white,
+                        child: Image.network(
+                          images[index]['src']['tiny'],
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              loadmore();
+            },
+            child: Container(
+              height: 60,
+              width: double.infinity,
+              color: Colors.black,
+              child: Center(
+                child: Text('Load More',
+                    style: TextStyle(fontSize: 20, color: Colors.white)),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
